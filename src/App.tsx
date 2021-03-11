9e3965a15bbf8f0481a50974550a8a6b592d6df6
Jake <napperjlg@gmail.com>
2021-03-11 15:25:54 +0000

    basic app css with header and body

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Body, Header } from './AppStyle';
 
 const App = () => {
   return (
-    <h1>What's On</h1>
+    <React.Fragment>
+      <Header>
+        <h1 className="ml2">What's On?</h1>
+      </Header>
+      <Body>
+        <h2>Browse through our list of films showing at the cinema right now!</h2>
+      </Body>
+
+    </React.Fragment>
+
+
   );
 }
 
---
+++ src/AppStyle.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  height: 100px;
+  background-color: #2f3030;
+  color: white;
+  display: flex;
+`;
+
+export const Body = styled.body`
+  display: flex;
+  justify-content: center;
+`;
