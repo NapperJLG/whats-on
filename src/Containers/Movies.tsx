34d29797af16fd0a9014922c3bcc8fb8d673d93c
Jake <napperjlg@gmail.com>
2021-03-13 16:42:03 +0000

    implemented a sort dropdown in the movies page

--- src/Containers/Movies.tsx
+++ src/Containers/Movies.tsx
@@ -8,14 +8,15 @@ import { formatMovie } from '../utils';
 export const Movies = () => {
 
     const [movieData, setMovieData] = React.useState<Movie[] | null>(null)
+    const [sortQueryString, setSortQueryString] = React.useState<string>('')
 
     React.useEffect(() => {
-        axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=bfc5fbd434c06c5ce7d01d69b041f090').then((response) => {
+        axios.get('https://api.themoviedb.org/3/discover/movie?api_key=bfc5fbd434c06c5ce7d01d69b041f090&language=en-US&primary_release_date.gte=2021-01-29&primary_release_date.lte=2021-03-18&with_release_type="2|3' + sortQueryString).then((response) => {
             setMovieData(response.data.results.map((movie: any) => formatMovie(movie)))
         })
-    }, [])
+    }, [sortQueryString])
 
     return (
-        <MoviesPage movieData={movieData} />
+        <MoviesPage movieData={movieData} setSortQueryString={setSortQueryString} />
     )
 }
--- src/Pages/Movies.tsx
+++ src/Pages/Movies.tsx
@@ -4,19 +4,36 @@ import { Dropdown } from '../Components/Dropdown/Dropdown';
 import { MovieCard } from '../Components/MovieCard/MovieCard';
 import { Movie } from '../types';
 
-export const MoviesPage = (props: { movieData: Movie[] | null }) => {
+export const MoviesPage = (props: { movieData: Movie[] | null; setSortQueryString: React.Dispatch<React.SetStateAction<string>> }) => {
 
-    const filterList = ["Rating (Asc)", "Rating (Desc)", "Title (A-Z)", "Title (Z-A)"]
+    const filterList = ["Rating (Asc)", "Rating (Desc)", "Popularity (Asc)", "Popularity (Desc)"]
+
+    const applySort = (sortSelection: string) => {
+        switch (sortSelection) {
+            case "Rating (Asc)":
+                props.setSortQueryString("&sort_by=vote_average.asc")
+                break;
+            case "Rating (Desc)":
+                props.setSortQueryString("&sort_by=vote_average.desc")
+                break;
+            case "Popularity (Asc)":
+                props.setSortQueryString("&sort_by=popularity.asc")
+                break;
+            case "Popularity (Desc)":
+                props.setSortQueryString("&sort_by=popularity.desc")
+                break;
+        }
+    }
 
     return (
 
         <React.Fragment>
             <h2>Browse through our list of films showing at the cinema right now!</h2>
             <div className="self-start col col-2">
-                <Dropdown list={filterList} callback={(item: string) => { }} />
+                <Dropdown list={filterList} callback={(item: string) => { applySort(item) }} />
             </div>
             <MovieCards className="mt3">
-                {props.movieData?.map((movie: Movie, i: number) => {
+                {props.movieData && props.movieData.map((movie: Movie, i: number) => {
                     return (
                         <MovieCard key={"movieCard_" + i} movie={movie} />
                     )
